4d11b306a75afe6e7b19709092d4143f2f880c4e
ajay-mahara <169141125ram@gmail.com>
2025-01-15 11:00:55 +0545

    Update script.js

--- script.js
+++ script.js
@@ -7,20 +7,4 @@ themeToggle.addEventListener("click", () => {
 });
 // script.js
 
-// Function to update the time
-function updateClock() {
-    const now = new Date();
-    let hours = now.getHours();
-    let minutes = now.getMinutes();
-    let seconds = now.getSeconds();
 
-    hours = (hours < 10) ? '0' + hours : hours;
-    minutes = (minutes < 10) ? '0' + minutes : minutes;
-    seconds = (seconds < 10) ? '0' + seconds : seconds;
-
-    const timeString = `${hours}:${minutes}:${seconds}`;
-    document.getElementById('clock').textContent = timeString;
-}
-
-updateClock();
-setInterval(updateClock, 1000);

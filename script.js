63cd2fd358726520b9ca6c4bfd6743c5099cce13
ajay-mahara <169141125ram@gmail.com>
2025-01-17 23:23:38 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Dark Mode Toggle
-const themeToggle = document.getElementById("themeToggle");
-
-themeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    themeToggle.textContent = document.body.classList.contains("dark-mode") ? "☀" : "☾";
-});
-// script.js
-
 
